db8c76975b83519177e4f3394f5ee1d54fc303b0
Bekki Young <moonbladez@hotmail.com>
2020-02-13 19:56:11 +0100

    individual movie view added

--- index.js
+++ index.js
@@ -73,9 +73,63 @@ const onInput = async event => {
 
 
 //once person has selected movie from drop down
-const onMovieSelect = (movie) => {
-    console.log(movie)
+const onMovieSelect = async movie => {
+    //follow up query based on individual ID 
+    const response = await axios.get("http://www.omdbapi.com/", {
+        params: {
+            apikey: "f0d20119",
+            i: movie.imdbID
+        }
+    });
+
+    document.querySelector("#summary").innerHTML = movieTemplate(response.data)
 }
+
+//html build up for the detailed movie data
+const movieTemplate = (movieDetail) => {
+    return `
+    <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetail.Poster}">
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <h2>${movieDetail.Title}</h2>
+        <h3>${movieDetail.Genre}</h3>
+        <p>${movieDetail.Plot}</p>
+      </div>
+    </div>
+  </article>
+  <article class="notification is-secondary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+  <article class="notification is-secondary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+  <article class="notification is-secondary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+  </article>
+  <article class="notification is-secondary">
+    <p class="title">${movieDetail.imdbRating}</p> 
+    <p class="subtitle">IMDB Rating</p> 
+  </article>
+  <article class="notification is-secondary">
+    <p class="title">${movieDetail.imdbVotes}</p> 
+    <p class="subtitle">IMDB Votes</p> 
+  </article>
+
+    `
+
+
+}
+
+
+
 
 
 
